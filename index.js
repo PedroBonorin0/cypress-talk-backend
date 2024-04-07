ca8c6f1208e90f64b942f58d3736daee31eccb0a
PedroBonorin0 <pepebonorinobraga@gmail.com>
2024-04-07 19:02:27 -0300

    first comimt

---
+++ index.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = process.env.PORT || 8080;
+
+const Todo = require('./models/todo.model');
+
+app.use(cors({
+  origin: '*'
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/api/todos', async (req, res) => {
+  if(!req.body.name)
+    res.send('No name provided').status(400);
+
+  try {
+    const todo = await Todo.create(req.body);
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.deleteOne({_id: req.params.id})
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+  console.log('Connected to database!');
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
---
+++ models/todo.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = Todo;
